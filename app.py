fc101307875ac192043366de3df57b7ae3a864fa
jaolsen7 <jaolsen@berkeley.edu>
2023-01-12 15:48:48 -0800

    testing routes, refactored app a tad

--- app.py
+++ app.py
@@ -4,70 +4,77 @@ import os
 import psycopg2
 
 load_dotenv()
-
-app = Flask(__name__)
-
 url = os.getenv("DB_URL")
 
-connection = psycopg2.connect(url)
+def create_app():
 
-CREATE_ENCLOSURES_TABLE = (
-    "CREATE TABLE IF NOT EXISTS enclosures (id SERIAL PRIMARY KEY, name TEXT);"
-)
+    app = Flask(__name__)
 
-CREATE_ANIMALS_TABLE = (
-    "CREATE TABLE IF NOT EXISTS animals (id SERIAL PRIMARY KEY, name TEXT, quantity INTEGER, enclosure_id INTEGER, FOREIGN KEY(enclosure_id) REFERENCES enclosures(id) ON DELETE CASCADE);"
-)
+    connection = psycopg2.connect(url)
 
-INSERT_ENCLOSURE_RETURN_ID = "INSERT INTO enclosures (name) VALUES (%s) RETURNING id;"
-INSERT_ANIMAL = (
-    "INSERT INTO animals (name, quantity, enclosure_id) VALUES (%s, %s, %s);"
-)
+    CREATE_ENCLOSURES_TABLE = (
+        "CREATE TABLE IF NOT EXISTS enclosures (id SERIAL PRIMARY KEY, name TEXT);"
+    )
 
-GET_ONE_ENCLOSURE = "SELECT name FROM enclosures WHERE id = (%s)"
+    CREATE_ANIMALS_TABLE = (
+        "CREATE TABLE IF NOT EXISTS animals (id SERIAL PRIMARY KEY, name TEXT, quantity INTEGER, enclosure_id INTEGER, FOREIGN KEY(enclosure_id) REFERENCES enclosures(id) ON DELETE CASCADE);"
+    )
 
-GET_HABITAT_BY_ID = "SELECT enclosures.name, animals.id, animals.name, animals.quantity FROM enclosures INNER JOIN animals ON enclosures.id=animals.enclosure_id WHERE enclosures.id = (%s);"
-GET_HABITAT_BY_NAME = "SELECT enclosures.name, animals.id, animals.name, animals.quantity FROM enclosures INNER JOIN animals ON enclosures.id=animals.enclosure_id WHERE enclosures.name = (%s);"
+    INSERT_ENCLOSURE_RETURN_ID = "INSERT INTO enclosures (name) VALUES (%s) RETURNING id;"
+    INSERT_ANIMAL = (
+        "INSERT INTO animals (name, quantity, enclosure_id) VALUES (%s, %s, %s);"
+    )
 
-@app.get("/")
-def home():
-    return "Welcome to the Safari Park! Hope you're not afraid of lions, tigers, bears, and much more!"
+    GET_ONE_ENCLOSURE = "SELECT name FROM enclosures WHERE id = (%s)"
 
-@app.post("/api/enclosure")
-def create_enclosure():
-    data = request.get_json()
-    name = data["name"]
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute(CREATE_ENCLOSURES_TABLE)
-            cursor.execute(INSERT_ENCLOSURE_RETURN_ID, (name,))
-            enclosure_id = cursor.fetchone()[0]
-        return {"id": enclosure_id, "message": f"{name} habitat created."}, 201
+    GET_HABITAT_BY_ID = "SELECT enclosures.name, animals.id, animals.name, animals.quantity FROM enclosures INNER JOIN animals ON enclosures.id=animals.enclosure_id WHERE enclosures.id = (%s);"
+    GET_HABITAT_BY_NAME = "SELECT enclosures.name, animals.id, animals.name, animals.quantity FROM enclosures INNER JOIN animals ON enclosures.id=animals.enclosure_id WHERE enclosures.name = (%s);"
 
-@app.post("/api/animal")
-def add_animal():
-    data = request.get_json()
-    name = data["name"]
-    quantity = data["quantity"]
-    enclosure_id = data["enclosure_id"]
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute(CREATE_ANIMALS_TABLE)
-            cursor.execute(INSERT_ANIMAL, (name, quantity, enclosure_id))
-        return {"message": "New species added!"}, 201
+    @app.get("/")
+    def home():
+        return "Welcome to the Safari Park! Hope you're not afraid of lions, tigers, bears, and much more!"
 
-@app.get("/api/habitat/<enclosure_id_or_name>")
-def get_habitat(enclosure_id_or_name):
-    with connection:
-        with connection.cursor() as cursor:
-            if request.view_args['enclosure_id_or_name'].isdigit():
-                cursor.execute(GET_HABITAT_BY_ID, (enclosure_id_or_name,))
-            else:
-                name = request.view_args['enclosure_id_or_name'].replace("-", " ")
-                cursor.execute(GET_HABITAT_BY_NAME, (name,))
-            animals = []
-            habitat = ""
-            for result in cursor:
-                habitat = result[0]
-                animals.append({"animal_id": result[1], "name": result[2], "quantity": result[3]})
-        return {habitat: animals}, 201
+    @app.post("/api/enclosure")
+    def create_enclosure():
+        data = request.get_json()
+        name = data["name"]
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(CREATE_ENCLOSURES_TABLE)
+                cursor.execute(INSERT_ENCLOSURE_RETURN_ID, (name,))
+                enclosure_id = cursor.fetchone()[0]
+            return {"id": enclosure_id, "message": f"{name} habitat created."}, 201
+
+    @app.post("/api/animal")
+    def add_animal():
+        data = request.get_json()
+        name = data["name"]
+        quantity = data["quantity"]
+        enclosure_id = data["enclosure_id"]
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(CREATE_ANIMALS_TABLE)
+                cursor.execute(INSERT_ANIMAL, (name, quantity, enclosure_id))
+            return {"message": "New species added!"}, 201
+
+    @app.get("/api/habitat/<enclosure_id_or_name>")
+    def get_habitat(enclosure_id_or_name):
+        with connection:
+            with connection.cursor() as cursor:
+                if request.view_args['enclosure_id_or_name'].isdigit():
+                    cursor.execute(GET_HABITAT_BY_ID, (enclosure_id_or_name,))
+                else:
+                    name = request.view_args['enclosure_id_or_name'].replace("-", " ")
+                    cursor.execute(GET_HABITAT_BY_NAME, (name,))
+                animals = []
+                habitat = ""
+                for result in cursor:
+                    habitat = result[0]
+                    animals.append({"animal_id": result[1], "name": result[2], "quantity": result[3]})
+            return {habitat: animals}, 201
+    return app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- test_main.py
+++ test_main.py
@@ -1,36 +1,33 @@
-# import sys
-# sys.path.insert(1, './code')
 import pytest
 import psycopg2
-# import sqlite3
-# import requests
 import os
-from app import url, GET_HABITAT
+from app import url, create_app
 
 @pytest.fixture
 def setup_database():
     """ Fixture to set up the in-memory database with test data """
     conn = psycopg2.connect(url)
+    print(conn.info)
     cursor = conn.cursor()
+    print(cursor)
     cursor.execute('''
 	    CREATE TABLE IF NOT EXISTS enclosures
         (id INT, name TEXT)''')
     sample_data = [
-        (1, 'desert'),
-        (2, 'reef'),
+        (2, 'rain forest'),
+        (3, 'reef'),
     ]
     cursor.execute('''
 	    CREATE TABLE IF NOT EXISTS animals
         (id INT, name TEXT, quantity INT, enclosure_id INT)''')
     sample_data2 = [
-        (1, 'snake', 4, 1),
-        (2, 'scorpion', 10, 1),
-        (3, 'seabass', 12, 2),
+        (3, 'toucan', 4, 2),
+        (4, 'monkey', 10, 2),
+        (5, 'seabass', 12, 3),
     ]
     cursor.executemany('INSERT INTO enclosures VALUES(%s, %s)', sample_data)
     cursor.executemany('INSERT INTO animals VALUES(%s, %s, %s, %s)', sample_data2)
     yield conn, cursor
-
 
 def test_connection(setup_database):
     # Test to make sure that there are items in each database
@@ -44,7 +41,58 @@ def test_connection(setup_database):
     print(result2)
     assert len(list(result2)) == 3
 
+@pytest.fixture
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+    yield app
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+def test_request_home(client):
+    print("HOME get request")
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Welcome to the Safari Park! Hope you're not afraid of lions, tigers, bears, and much more!" in response.data
+
+def test_request_habitat(client):
+    print("HABITAT get request")
+    response = client.get("/api/habitat/1")
+    json_response = response.get_json()
+    assert response.status_code == 201
+    assert json_response == {'desert': [{'animal_id': 1, 'name': 'snake', 'quantity': 4}, {'animal_id': 2, 'name': 'scorpion', 'quantity': 10}]}
+
+@pytest.mark.skip
 def test_file_exists():
     # Test to make sure all necessary files exists
     assert os.path.exists('./requirements.txt')
     assert os.path.exists('./app.py')
+
+## NOT TDD tests
+# def test_get_home():
+#     # Testing home route
+#     response = app.test_client().get('/')
+#     assert response.status_code == 200
+#     assert response.data == b"Welcome to the Safari Park! Hope you're not afraid of lions, tigers, bears, and much more!"
+
+# def test_get_habitat():
+#     # Testing habitat get request
+#     with app.test_client() as connection:
+#         response = connection.get('/api/habitat/desert')
+#         json_response = response.get_json()
+#         assert response.status_code == 201
+#         assert json_response == {'desert': [{'animal_id': 1, 'name': 'snake', 'quantity': 4}, {'animal_id': 2, 'name': 'scorpion', 'quantity': 10}]}
+
+# def test_post_enclosure():
+#     # Testing enclosure post request
+#     with app.test_client() as connection:
+#         response = connection.post('/api/enclosure', json={
+#             "name": "reef"
+#         })
+#         json_response = response.get_json()
+#         assert response.status_code == 201
+#         assert json_response == {'id': 6, 'message': 'reef habitat created.'}
